d454de6e5f4a035674045c029f34403b4823c7bc
VJ-2303 <vanaraj1018@gmail.com>
2026-01-11 15:19:32 +0530

    Created helper function for writing JSON Responses with status code and custom headers, and used that helper function to send health informations

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", "1.0.0")
+	data := envelope{
+		"system": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     "1.0.0",
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem", http.StatusInternalServerError)
+	}
 }
