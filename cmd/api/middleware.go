f80ceb6b6972a5dc2be2c26ef8078aef010c52e2
VJ-2303 <vanaraj1018@gmail.com>
2026-01-27 19:42:23 +0530

    Added cors Middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -56,3 +56,18 @@ func (app *application) requireAuthenticatedUser(next http.HandlerFunc) http.Han
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -21,5 +21,5 @@ func (app *application) router() http.Handler {
 	mux.HandleFunc("GET /v1/users/activate", app.activateUserHandler)
 	mux.HandleFunc("GET /v1/users/profile", app.requireAuthenticatedUser(app.getProfileHandler))
 
-	return app.authenticate(mux)
+	return app.enableCORS(app.authenticate(mux))
 }
